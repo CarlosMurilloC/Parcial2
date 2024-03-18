f0ed83f3658e08655dc67d5138e9ac8d6e4d08c7
CarlosMurilloC <thecamc88@gmail.com>
2024-03-18 10:58:24 +0100

    Agregadas constantes para mensajes de gestión de empleados

---
+++ TEXTOS.java
@@ -0,0 +1,4 @@
+public class Textos {
+    public static final String MENSAJE_PORCENTAJE_AUMENTO = "Introduzca el porcentaje de aumento de salario: ";
+    public static final String MENSAJE_LISTA_EMPLEADOS = "Lista de Empleados:";
+}
